e8f4cc94dd7f6bc2c3dbc593f2177043a4a8721a
md abu hanif mia <mohammadbitullah@gmail.com>
2026-01-06 03:05:37 +0600

    update cors

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors');  // update cors
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
